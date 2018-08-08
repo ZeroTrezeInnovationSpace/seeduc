05074b171515840e20fcd798be7b9a704989327a
Rodrigo Garcia Topan Moreira <rodrigo@gruporesolv.com.br>
2018-08-08 15:19:28 -0300

    Atualizacao De Senhas - Usuário - Sem Email

--- resources/views/auth/verifyRememberPassword.blade.php
+++ resources/views/auth/verifyRememberPassword.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.register') 
 
 @section('content')
-&nbsp;<a href="/seeduc"><img src="https://marketplace.canva.com/MAB4qEAhS1M/1/thumbnail_large/canva-directional-arrow-icon-MAB4qEAhS1M.png" style="max-width:100px;
+    &nbsp;<a href="/seeduc"><img src="https://marketplace.canva.com/MAB4qEAhS1M/1/thumbnail_large/canva-directional-arrow-icon-MAB4qEAhS1M.png" style="max-width:100px;
 max-height:50px;"></a>
 <br> &ensp; <font style="color: green;">Voltar</font>
 <center>
-        <h2 class="display-3 lead" style="margin: 90px;">Verificação de informações cadastrais para redefinição de senha </h2>
+        <h2 class="display-4 lead" style="margin: 90px;">Verificação de informações cadastrais para redefinição de senha </h2>
+        1 - Informar seu CPF e email utilizados no cadastro <br>
+                2 - Realizar tentativa de login novamente no sistema de gerenciamento de eventos do SEEDUC. <br> 
+                3 - Você será redirecionado(a) para página de redefinição de senha. Informe sua nova senha. <br>
+                4 - Salve a nova senha. Através do botão salvar alterações <br> 
+                5 - Realize login novamente com a senha definida. <br>
+                <br>
 </center>
 @if( session()->has('error') )
 <br><br>
@@ -18,6 +24,7 @@ max-height:50px;"></a>
 
 <div class="center1"> 
         <div id="CPFverification">
+
                 <form method="POST" action="{{ route('verify_remember_password') }}">  
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">    
                         <input placeholder="CPF" style="box-shadow: 1px -1px 69px 0px rgba(27, 126, 27, 0.51);" class="form-control" type="text" name="CPF" onkeydown="javascript: fMasc( this, mCPF );" required> <br>
--- resources/views/mail/rememberPassword.blade.php
+++ resources/views/mail/rememberPassword.blade.php
@@ -3,10 +3,11 @@
         Prezado(a), {{ $name }} <br>
         A solicitação de redifinição de senha foi processada com sucesso.
         Lista de passos para redefinição de senha <br>
-        1 - Favor realizar tentativa de login novamente no sistema de gerenciamento de eventos do SEEDUC. <br> 
-        2 - Você será redirecionado(a) para página de redefinição de senha. Informe sua nova senha. <br>
-        3 - Salve a nova senha. Através do botão salvar <br> 
-        4 - Realize login novamente com a senha definida. <br>
+        1 - Informar seu CPF e email utilizados no cadastro <br>
+        2 - Realizar tentativa de login novamente no sistema de gerenciamento de eventos do SEEDUC. <br> 
+        3 - Você será redirecionado(a) para página de redefinição de senha. Informe sua nova senha. <br>
+        4 - Salve a nova senha. Através do botão salvar alterações <br> 
+        5 - Realize login novamente com a senha definida. <br>
     </p>
    Atenciosamente,<br>
    Zero Treze Innovation Space Center
