4c5cd06a2c32492523ebf34ee3c123c79d048351
Vinicius Simone Martins <vinicius@gruporesolv.com.br>
2018-08-10 14:02:43 -0300

    Cointeção de erro 3.1

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -169,7 +169,10 @@ public function show($id)
         }
 
         public function verifyCapacity($activity_id){
-
+            if($activity_id == 14 || $activity_id == 152 || $activity_id == 85 || $activity_id == 159 ){
+                return 1;
+            }
+            else{
                 $activity = Activity::where('id', $activity_id)->get();
                 $subscriptions = Subscription::where('activity_id', $activity_id)->count();
                 $full = 0;
@@ -177,7 +180,7 @@ public function show($id)
                     $full++;
                 }
                 return $full;
-            
+            }
         }
 
     }
