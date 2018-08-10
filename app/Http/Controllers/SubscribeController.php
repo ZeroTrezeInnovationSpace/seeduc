3c5ca2f22c3971bbfd8a49577905717610141595
Vinicius Simone Martins <vinicius@gruporesolv.com.br>
2018-08-10 13:27:27 -0300

    Cointeção de erro 2.0

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -169,7 +169,7 @@ public function show($id)
         }
 
         public function verifyCapacity($activity_id){
-            if($activity_id == 14){
+            if($activity_id == 14 || $activity_id = 152 || $activity_id = 85 || $activity_id = 159 ){
                 return 1;
             }
             else{
