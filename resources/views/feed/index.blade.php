bfb5b24e5359b4a09d5c4bdc09e70d4489b0fe0e
Vinicius Simone Martins <vinicius@gruporesolv.com.br>
2018-08-13 09:42:25 -0300

    Arrumar horário Noite 2

--- resources/views/feed/index.blade.php
+++ resources/views/feed/index.blade.php
@@ -91,7 +91,7 @@
         @elseif($activity->period == 'VLItarde')
         <td>13h30 às 17h30</td>
         @elseif($activity->period == 'noite')
-        <td>{{$activity->period}}</td>
+        <td>19h às 22h</td>
         @endif
         <td>{{$activity->location->name}} - {{$activity->location->full_adress}} 
           {{$activity->location->adress_number}}</td>
